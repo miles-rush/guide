b3b12e2b5a701cd881170578b474ea58b521b35b
Persistr <755842816@qq.com>
2020-03-14 11:14:56 +0800

    更改了注册的返回数据

--- src/main/java/com/miles/demo/UserController.java
+++ src/main/java/com/miles/demo/UserController.java
@@ -12,23 +12,30 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    static ResponseCode REGISTER_OK = new ResponseCode(1,"register success");
+    static ResponseCode REGISTER_FAIL = new ResponseCode(0,"register fail");
+    static ResponseCode REGISTER_FAIL_REPEAT = new ResponseCode(0,"register fail account repeat");
     @PostMapping(value = "/user/register")
-    private User register(@RequestParam("account") String account,@RequestParam String password) {
+    private ResponseCode register(@RequestParam("account") String account,@RequestParam String password) {
         if (userRepository.findByUserAccount(account) != null) {
-            return null;
+            return REGISTER_FAIL_REPEAT;
         }
         User user = new User();
         user.setAccount(account);
         user.setPassword(password);
         user.setAdmin(false);
 
-        return userRepository.save(user);
+        if (userRepository.save(user) != null) {
+            return REGISTER_OK;
+        }else {
+            return REGISTER_FAIL;
+        }
     }
 
     static ResponseCode LOGIN_OK = new ResponseCode(1,"login success");
     static ResponseCode LOGIN_FAIL = new ResponseCode(0,"login fail");
     static ResponseCode LOGIN_OK_ADMIN = new ResponseCode(2,"login success admin");
-    @GetMapping(value = "/user/login")
+    @PostMapping(value = "/user/login")
     private ResponseCode login(@RequestParam("account") String account,@RequestParam String password) {
         User user = userRepository.findByUserAccountAndPassword(account,password);
         if (user != null) {
